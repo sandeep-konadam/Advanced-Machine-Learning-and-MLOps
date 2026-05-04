61b6277119ffe6dd3a0b41ca017d143f26d0350e
sandeep-konadam <sandeepkonadam@gmail.com>
2026-05-04 22:49:14 +0530

    Changes for the relative paths

--- hosting/hosting.py
+++ hosting/hosting.py
@@ -8,7 +8,7 @@ SPACE_REPO = "sandy1916/Tourism-Prediction-Space"
 
 # Push all deployment files to the Hugging Face Space
 api.upload_folder(
-    folder_path="Advanced-Machine-Learning-and-MLOps/deployment",
+    folder_path="deployment",
     repo_id=SPACE_REPO,
     repo_type="space",
     path_in_repo="",
--- model_building/data_register.py
+++ model_building/data_register.py
@@ -19,7 +19,7 @@ except RepositoryNotFoundError:
 
 # Upload the data folder to the HF dataset hub
 api.upload_folder(
-    folder_path="Advanced-Machine-Learning-and-MLOps/data",
+    folder_path="data",
     repo_id=repo_id,
     repo_type=repo_type,
 )
